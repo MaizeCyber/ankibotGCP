984d37677efe89b565e8e0fc9b1bca619fadceb8
Maize Cyber <maizecyber@pm.me>
2026-01-22 21:00:11 -0500

    Fix alert channel title syntax #deploy

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -1,11 +1,11 @@
-resource "google_pubsub_topic" "ankibot_idle_topic" {
-  name = "Ankibot idel alert topic"
+resource "google_pubsub_topic" "ankibot-idle-topic" {
+  name = "Ankibot idle alert topic"
 }
 
 resource "google_monitoring_notification_channel" "pubsub_channel" {
   display_name = "PubSub Shutdown Channel"
   type         = "pubsub"
   labels = {
-    topic = google_pubsub_topic.ankibot_idle_topic.id
+    topic = google_pubsub_topic.ankibot-idle-topic.id
   }
 }
