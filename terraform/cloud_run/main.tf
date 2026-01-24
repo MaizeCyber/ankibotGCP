fc2e6da7eb366a8d4a3ee6828d33d7233dce2573
Maize Cyber <maizecyber@pm.me>
2026-01-24 14:21:33 -0500

    Try original metric against #deploy

--- terraform/cloud_run/main.tf
+++ terraform/cloud_run/main.tf
@@ -8,7 +8,7 @@ resource "google_cloud_run_v2_service" "ankibot" {
   template {
     service_account = var.sa_email
     scaling {
-      max_instance_count = 4
+      max_instance_count = 3
       min_instance_count = 0
     }
     containers {
